6058ce677b83717731816f17234e9b99b17404d9
XiaoMing <1424408824@qq.com>
2019-06-21 16:53:46 +0800

    vuex

---
+++ src/components/index/dx-index-header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="header">
+    123
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dx-index-header',
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+  .header{
+    font-size: 20px;
+  }
+</style>
---
+++ src/page/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="">
+    <dx-index-header />
+  </div>
+</template>
+
+<script>
+import DxIndexHeader from '../components/index/dx-index-header'
+export default {
+  name: 'Index',
+  components: {
+    DxIndexHeader
+  },
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+</style>
