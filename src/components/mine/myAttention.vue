6b66e4bcdf3ad01d2f96cc32adc12a0d3fa3ab67
e <1771244617@qq.com>
2019-07-02 17:22:02 +0800

    随v吧

---
+++ src/components/mine/myAttention.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="myAttention">
+    <dxHeaderReturn :headerReturnTitle="headerReturnTitle"></dxHeaderReturn>
+    <dxDefaultPage v-slot:DefaultPageLogo :DefaultPageTxt="DefaultPageTxt">
+      <img class="imgAuto" src="../../assets/img/defaultPage/ic_doctor_empty.png" alt="">
+    </dxDefaultPage>
+  </div>
+</template>
+
+<script>
+  import dxHeaderReturn from '../../components/public/dxHeaderReturn'
+  import dxDefaultPage from '../../components/public/dxDefaultPage'
+
+  /**
+   * 我的关注
+   */
+  export default {
+    name: "myAttention",
+    data() {
+      return {
+        headerReturnTitle: {
+          title: "我关注的医生"
+        },
+        DefaultPageTxt: {
+          txt: "暂无关注的医生"
+        }
+      }
+    },
+    components: {
+      dxHeaderReturn, dxDefaultPage
+    }
+  }
+</script>
+
+<style scoped>
+  .myAttention {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+</style>
---
+++ src/components/mine/myCollect.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="myCollect">
+    <dxHeaderReturn :headerReturnTitle="headerReturnTitle"></dxHeaderReturn>
+    <dxDefaultPage v-slot:DefaultPageLogo :DefaultPageTxt="DefaultPageTxt">
+      <img class="imgAuto" src="../../assets/img/defaultPage/ic_collection_empty.png" alt="">
+    </dxDefaultPage>
+  </div>
+</template>
+
+<script>
+  import dxHeaderReturn from '../../components/public/dxHeaderReturn'
+  import dxDefaultPage from '../../components/public/dxDefaultPage'
+
+  export default {
+    name: "myCollect",
+    components: {
+      dxHeaderReturn, dxDefaultPage
+    },
+    data() {
+      return {
+        headerReturnTitle: {
+          title: "我的收藏"
+        },
+        DefaultPageTxt: {
+          txt: "暂无收藏内容"
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .myCollect {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
